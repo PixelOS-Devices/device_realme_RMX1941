af72c3f2dcbc4da21f4fced0ee365cda2e1ec0cc
TheMalachite <eliasgheeraert@gmail.com>
2021-11-28 14:07:38 +0530

    RMX1941: Patchelf few camera blobs to load VNDK-29 version of libutils
    Fixes:-
    
    10-08 10:32:53.058  1309  1309 F linker  : CANNOT LINK EXECUTABLE /vendor/bin/hw/camerasloganserver: cannot locate symbol _ZN7android11PropertyMapD1Ev referenced by /vendor/lib64/libmtkcam_stdutils.so...
    
    Change-Id: Iee9ddd58fe406dded4818c7e58f15ff0b9c14bea
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,12 @@ function blob_fixup() {
         vendor/bin/hw/android.hardware.wifi@1.0-service-lazy-mediatek)
             "${PATCHELF}" --replace-needed "libwifi-hal.so" "libwifi-hal-mtk.so" "${2}"
             ;;
+        vendor/bin/hw/camerasloganserver)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v29.so" "${2}"
+            ;;
+        vendor/lib/libmtkcam_stdutils.so|vendor/lib64/libmtkcam_stdutils.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v29.so" "${2}"
+            ;;
     esac
 }
 
