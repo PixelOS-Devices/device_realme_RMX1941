46d634e832e3f041200a2d2d1d0cf4cb69d3a94b
Anton Hansson <hansson@google.com>
2022-02-23 16:33:09 +0000

    RMX1941: Remove PRODUCT_AAPT config This config strips unused drawable resources from the device to save space, but it is not particularly effective in practice (roughly ~30 MB across all partitions).
    Remove it to make the system image more generic.
    
    Bug: 118676832
    Test: make
    Change-Id: I3ae120773e793af6e7b5759d383b7fa9c65aeb48

--- device.mk
+++ device.mk
@@ -309,11 +309,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0.vendor
 
-# Screen density
-PRODUCT_AAPT_CONFIG := xxxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
-PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
