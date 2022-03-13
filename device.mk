55712282067762204a7be80862ea1034a03bae9d
Anoos Ragh <ok.ano0s6090@gmail.com>
2022-03-13 17:24:08 +0000

    RMX1941:  Build android.hardware.health@2.0-impl-default.so
    Signed-off-by: Anoos Ragh <ok.ano0s6090@gmail.com>

--- device.mk
+++ device.mk
@@ -124,8 +124,9 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
+     android.hardware.health@2.0-impl-default.so \
     android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1-service 
 
 # GPS
 PRODUCT_PACKAGES += \
